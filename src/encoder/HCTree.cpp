3378c5be049c0b168db1331087b4608e1e6f1264
David <dayoun@ucsd.edu>
2020-02-11 12:27:22 +0000

    encoding

--- src/encoder/HCTree.cpp
+++ src/encoder/HCTree.cpp
@@ -6,16 +6,76 @@
 #include "HCTree.hpp"
 
 /* TODO */
-HCTree::~HCTree() {}
+HCTree::~HCTree() { root = nullptr; }
+
+priority_queue<HCNode*, vector<HCNode*>, HCNodePtrComp> pq;
 
 /* TODO */
-void HCTree::build(const vector<unsigned int>& freqs) {}
+void HCTree::build(const vector<unsigned int>& freqs) {
+    // creating a forest of single-node trees in the PQ
+    for (int i = 0; i < 256; i++) {
+        // creating nodes for each of the non-zero values
+        if (freqs.at(i) != 0) {
+            HCNode* n = new HCNode(freqs.at(i), i, nullptr, nullptr, nullptr);
+
+            // adding to leaves vector
+            leaves.push_back(n);
+
+            // adding to pq
+            pq.push(n);
+        } else {
+            // nullptr for symbols that dont appear
+            leaves.push_back(nullptr);
+        }
+    }
+    // no nodes in tree
+    if (pq.size() == 0) {
+        return;
+    }
+    while (pq.size() != 1) {
+        HCNode* n1 = pq.top();
+        pq.pop();
+        HCNode* n2 = pq.top();
+        pq.pop();
+
+        // creating parent node that takes c0's symbol
+        HCNode* p =
+            new HCNode(n1->count + n2->count, n1->symbol, n1, n2, nullptr);
+        // merge trees
+        n1->p = p;
+        n2->p = p;
+
+        // pushing the parent back into the queue
+        pq.push(p);
+    }
+    root = pq.top();
+    pq.pop();
+}
 
 /* TODO */
 // void HCTree::encode(byte symbol, BitOutputStream& out) const {}
 
 /* TODO */
-void HCTree::encode(byte symbol, ostream& out) const {}
+void HCTree::encode(byte symbol, ostream& out) const {
+    HCNode* curr = nullptr;
+    HCNode* prev = nullptr;
+    // string code = "";
+
+    curr = leaves.at(symbol);
+    while (curr != root) {
+        prev = curr;
+        curr = curr->p;
+
+        // checking if c0 or c1
+        if (curr->c0 == prev) {
+            // code = '0' + code;
+            out << '0';
+        } else {
+            // code = '1' + code;
+            out << '1';
+        }
+    }
+}
 
 /* TODO */
 // byte HCTree::decode(BitInputStream& in) const { return ' '; }
